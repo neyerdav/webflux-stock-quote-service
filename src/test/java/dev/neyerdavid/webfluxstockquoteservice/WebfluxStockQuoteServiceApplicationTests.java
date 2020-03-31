ba161f77f790a3bdfa34a2c8a033a05b83e15087
neyerd <david.neyer@asap.de>
2020-03-31 13:57:34 +0200

    Initial commit

---
+++ src/test/java/dev/neyerdavid/webfluxstockquoteservice/WebfluxStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.neyerdavid.webfluxstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxStockQuoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
